61d0a5f9894c7999617885f277ca6bfce852abb3
kemaphi <kevin.haun@live.de>
2023-04-14 11:32:26 +0200

    Scatterplot CalcDT

--- Python/Projekt_R_Python/Projekt_R_Python/Scatterplot.py
+++ Python/Projekt_R_Python/Projekt_R_Python/Scatterplot.py
@@ -2,15 +2,15 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 
-def scatterplot_test():
+def scatterplot_test(table):
 
-    x = np.array([5,7,8,12,9,4,17,13,6,10])
-    y = np.array([55,67,89,105,71,58,92,77,45,23])
+    x = table['Created Date']
+    y = table['CycleDays']
     plt.scatter(x, y, color = 'blue')
 
-    x = np.array([2,3,8,15,16,5,11,14,9,14])
-    y = np.array([107,90,66,86,130,98,100,76,67,88])
-    plt.scatter(x, y, color = 'red')
+    #x = np.array([2,3,8,15,16,5,11,14,9,14])
+    #y = np.array([107,90,66,86,130,98,100,76,67,88])
+    #plt.scatter(x, y, color = 'red')
 
     plt.show()
 
