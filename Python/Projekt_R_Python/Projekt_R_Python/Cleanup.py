0b44633cee5b50b6a831989eb77a57497915385f
kemaphi <kevin.haun@live.de>
2023-04-21 17:02:12 +0200

    Lineare Regression im Scatterplot

--- Python/Projekt_R_Python/Projekt_R_Python/Cleanup.py
+++ Python/Projekt_R_Python/Projekt_R_Python/Cleanup.py
@@ -21,11 +21,10 @@ def cleanRAWTable(table):
     pd.set_option('display.max_columns', None)
     pd.set_option('display.width', None)
     pd.set_option('display.max_colwidth', 15)
-<<<<<<< Updated upstream
     print(df)
-=======
+
     #print(df3)
->>>>>>> Stashed changes
+
    
 
 
--- Python/Projekt_R_Python/Projekt_R_Python/Scatterplot.py
+++ Python/Projekt_R_Python/Projekt_R_Python/Scatterplot.py
@@ -13,18 +13,44 @@ def scatterplot_test(table):
     table = table.convert_dtypes()
     #print(table.iloc[0][10]) # Example 11.10.2022 09:49
 
-    test_string = datetime.strptime('11.10.2022 09:49', '%d.%m.%Y %H:%M')
+    #table['Closed Date Datetime'] = datetime.strptime(table['Closed Date'].values, '%d.%m.%Y %H:%M')
     
-    print(test_string)
-    print(type(test_string))
+    #print(table['Closed Date Datetime'])
+    datetime_list = []
+    for i in table['Closed Date']:
+        datetime_list.append(datetime.strptime(i, '%d.%m.%Y %H:%M'))
 
-    test_timestamp = datetime.timestamp(test_string)
-    print(test_timestamp)
+    table['Closed Date Datetime'] = datetime_list
 
-    x = table['Closed Date'] #Dtype is a string Question is how to change string to float?
-    y = table['CycleDays'] #Dtype is a flote
+    
+
+    timestamp_list = []
+    for i in table['Closed Date Datetime']:
+        timestamp_list.append(datetime.timestamp(i))
+
+    table['Closed Date Timestamp'] = timestamp_list
+
+    print(table.info())
+
    
-    #sns.regplot(x = x, y = y, data = table[['Closed Date', 'CycleDays']])
+
+    x = list(table['Closed Date Timestamp']) #Dtype is a string Question is how to change string to float?
+    y = list(table['CycleDays']) #Dtype is a flote
+
+    slope, intercept, r, p, std_err = stats.linregress(x,y)
+
+    def myfunc(x):
+        return slope * x + intercept
+
+    mymodel = list(map(myfunc,x))
+
+    plt.scatter(x,y)
+    plt.plot(x,mymodel)
+    plt.show()
+
+    #sns.scatterplot(data= table, x='Closed Date Timestamp' ,y='CycleDays')
+   
+    #sns.regplot(data= table, x='Closed Date Timestamp' ,y='CycleDays')
 
     #plt.show()
 
